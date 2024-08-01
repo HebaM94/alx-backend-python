8c25d90f1ca2a75ea8ce37aef56f9f16d64e3641
Heba <hebamagde@gmail.com>
2024-08-02 01:41:31 +0300

    Task 2: Basic annotations - floor

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Annotated concatenation module"""
+""" Annotated concatenation module """
 
 
 def concat(str1: str, str2: str) -> str:
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Annotated floating module """
+from math import floor as fl
+
+
+def floor(n: float) -> int:
+    """Floor the float"""
+    return fl(n)
