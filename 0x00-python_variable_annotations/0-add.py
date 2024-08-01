4f7e54f1e6fae047b2e773cafa02a0a7f1324c72
Heba <hebamagde@gmail.com>
2024-08-02 01:36:28 +0300

    Task 1: Basic annotations - concat

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-""" Annotated summation module
-"""
+""" Annotated summation module"""
 
 
 def add(a: float, b: float) -> float:
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Annotated concatenation module"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate strings"""
+    return str1 + str2
