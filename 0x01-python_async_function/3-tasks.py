e716d5b51105444a980fc44156ccb289fd48809c
Heba <hebamagde@gmail.com>
2024-08-06 02:14:43 +0300

    Task 3: Tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Function for measuring runttime """
+import asyncio
+import time
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """ Takes an integer max_delay and returns a asyncio.Task
+        Args:
+            max_delay (int.): maximum delay time to await
+                       coroutine
+        Return: asyncio.Task
+    """
+    return asyncio.create_task(wait_random(max_delay))
